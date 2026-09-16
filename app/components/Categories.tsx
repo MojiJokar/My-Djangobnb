229693d99501c2fcd47702947c3ca838b7efef47
MaxJokar <mmb2020max@gmail.com>
2026-09-16 16:15:20 +0200

    opacity corrected

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,10 +1,34 @@
+// const Categories = () => {
+//     return (
+        
+//             <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+//                 <div 
+                    
+//                     className="`pb-4 flex flex-col items-center space-y-2 border-b-2 'border-white'  opacity-60  hover:opacity-100`">
+//                             asdll
+
+//                 </div>
+                
+                
+//             </div>
+
+
+
+
+//     )
+// }
+
+// export default Categories;
+
+
+
 const Categories = () => {
     return (
         
             <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
                 <div 
                     
-                    className="`pb-4 flex flex-col items-center space-y-2 border-b-2 'border-white'  opacity-60  hover:opacity-100`">
+                    className="`pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-40  hover:opacity-100">
                             asdll
 
                 </div>
@@ -19,9 +43,6 @@ const Categories = () => {
 }
 
 export default Categories;
-
-
-
 
 
 
