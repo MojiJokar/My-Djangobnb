9ca089cb296f149d567e95c95644bdb875735516
MaxJokar <mmb2020max@gmail.com>
2026-09-16 13:50:39 +0200

    navbar finished

--- app/components/navbar/searchFilters.tsx
+++ app/components/navbar/searchFilters.tsx
@@ -48,49 +48,50 @@
 
 const SearchFilters = () => {
     return (
-        <div className="flex flex-row items-center justify-between">
-            
-            {/* Where */}
-            <div className="cursor-pointer w-[250px] h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
-                <p className="text-xs font-semibold">Where</p>
-                <p className="text-sm">Wanted location</p>
-            </div>
+            <div className="flex flex-row items-center justify-between">
+                {/* <div className="hidden lg:block"> */}
+                {/* Where */}
+                    <div className="cursor-pointer w-[250px] h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Where</p>
+                        <p className="text-sm">Wanted location</p>
+                    </div>
 
-            {/* Check in */}
-            <div className="cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
-                <p className="text-xs font-semibold">Check in</p>
-                <p className="text-sm">Add dates</p>
-            </div>
+                    {/* Check in */}
+                    <div className="cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Check in</p>
+                        <p className="text-sm">Add dates</p>
+                    </div>
 
-            {/* Check out */}
-            <div className="cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
-                <p className="text-xs font-semibold">Check out</p>
-                <p className="text-sm">Add dates</p>
-            </div>
+                    {/* Check out */}
+                    <div className="cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Check out</p>
+                        <p className="text-sm">Add dates</p>
+                    </div>
 
-            {/* Who */}
-            <div className="cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
-                <p className="text-xs font-semibold">Who</p>
-                <p className="text-sm">Add guests</p>
-            </div>
+                    {/* Who */}
+                    <div className="cursor-pointer h-[48px] lg:h-[64px] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Who</p>
+                        <p className="text-sm">Add guests</p>
+                    </div>
 
-            {/* Search button */}
-            <div className="p-2">
-                <div className="cursor-pointer w-12 h-12 flex items-center justify-center bg-rose-500 hover:bg-rose-600 transition rounded-full text-white">
-                    <svg
-                        viewBox="0 0 32 32"
-                        className="w-4 h-4"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                        aria-hidden="true"
-                    >
-                        <path d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9" />
-                    </svg>
+                    {/* Search button */}
+                    <div className="p-2">
+                        <div className="cursor-pointer w-12 h-12 flex items-center justify-center bg-rose-500 hover:bg-rose-600 transition rounded-full text-white">
+                            <svg
+                                viewBox="0 0 32 32"
+                                className="w-4 h-4"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                                aria-hidden="true"
+                            >
+                                <path d="M13 24a11 11 0 1 0 0-22 11 11 0 0 0 0 22zm8-3 9 9" />
+                            </svg>
+                        </div>
+                    </div>
+
                 </div>
-            </div>
-
-        </div>
+            // </div>
     );
 };
 
