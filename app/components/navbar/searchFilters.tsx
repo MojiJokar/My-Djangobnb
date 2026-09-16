b4d5c965fdae20872a092fe0a905e4c98fa426bf
MaxJokar <mmb2020max@gmail.com>
2026-09-16 12:39:52 +0200

    where checkin check out with cursor created

--- app/components/navbar/searchFilters.tsx
+++ app/components/navbar/searchFilters.tsx
@@ -13,15 +13,90 @@
 
 
 
+// const SearchFilters = () => {
+   
+
+//     return (
+           
+//                 <div className="flex flex-row items-center justify-between">
+//                     <div className="cursor-pointer w-[250px] h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+//                         <p className="text-xs font-semibold">Where</p>
+//                         <p className="text-sm">Wanted location</p>
+//                     </div>
+
+//                     <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+//                         <p className="text-xs font-semibold">Check in</p>
+//                         <p className="text-sm">Add dates</p>
+//                     </div>
+                    
+//                     <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+//                         <p className="text-xs font-semibold">Check out</p>
+//                         <p className="text-sm">Add dates</p>
+//                     </div>
+
+//                     <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+//                         <p className="text-xs font-semibold">Who</p>
+//                         <p className="text-sm">Add guests</p>
+//                     </div>
+//                 </div>
+            
+//     )
+// }
+
+// export default SearchFilters;
+
+
+
+
+
+
 const SearchFilters = () => {
    
 
     return (
-        <>testtttt search filter</>
+           
+                <div className="flex flex-row items-center justify-between">
+                    <div className="cursor-pointer w-[250px] h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Where</p>
+                        <p className="text-sm">Wanted location</p>
+                    </div>
+
+                    <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Check in</p>
+                        <p className="text-sm">Add dates</p>
+                    </div>
+                    
+                    <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Check out</p>
+                        <p className="text-sm">Add dates</p>
+                    </div>
+
+                    <div className="cursor-pointer h-[48px] lg:h-[64] px-8 flex flex-col justify-center rounded-full hover:bg-gray-100">
+                        <p className="text-xs font-semibold">Who</p>
+                        <p className="text-sm">Add guests</p>
+                    </div>
+                </div>
+            
     )
 }
 
 export default SearchFilters;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
