0e9255b5c813705bea13bd8a121780848d2c5e7d
MaxJokar <mmb2020max@gmail.com>
2026-09-16 16:29:28 +0200

    first category icon done

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -20,31 +20,46 @@
 
 // export default Categories;
 
+// import Image from "next/image";
 
+// const Categories = () => {
+//     return (
+//         <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+//             <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-40 hover:opacity-100">
+//                 <Image
+//                     src="/icn_category_beach.jpeg"
+//                     alt="Category Beach"
+//                     width={20}
+//                     height={20}
+//                 />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Categories;
+
+
+
+
+import Image from "next/image";
 
 const Categories = () => {
     return (
-        
-            <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-                <div 
-                    
-                    className="`pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-40  hover:opacity-100">
-                            asdll
-
-                </div>
-                
-                
+        <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-40 hover:opacity-100">
+                <Image
+                    src="/icn_category_beach.jpeg"
+                    alt="Category Beach"
+                    width={20}
+                    height={20}
+                />
             </div>
-
-
-
-
-    )
-}
+        </div>
+    );
+};
 
 export default Categories;
-
-
 
 
 
